6399c106032e95f3cde478081e3058c101753d25
Alex Martin <alexbuzzbee@gmail.com>
2015-07-10 14:45:17 -0400

    Implement processCommand()

--- src/shell/03_cmdprocess.lua
+++ src/shell/03_cmdprocess.lua
@@ -5,6 +5,24 @@ local function getCommand()
   return dos("readTerm")
 end
 
-local function processCommand()
-
+local function processCommand(line)
+  local words = {}
+  for position, word in string.gmatch(line, "\\s(.*?)|(.*?)\\s") do -- Break the line up into words.
+    words[position] = word
+  end
+  local command = table.remove(words, 1) -- Get the command itself.
+  for internal, table in pairs(internalCommands) do -- Try internal commands.
+    if internal == command then
+      table.exec(words) -- Run the internal command.
+      return true -- Succede.
+    end
+  end
+  if not dos("execute", command, table.unpack(words)) then -- Try running the command as given...
+    if not dos("execute", command .. ".COM", table.unpack(words)) then -- ...as a .COM...
+      if not dos("execute", command .. ".EXE", table.unpack(words)) then -- ...and as a .EXE...
+      return false -- ...and fail if none of those work.
+      end
+    end
+  end
+  return true -- Succede.
 end
